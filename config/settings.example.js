cbacfb6d568f4780180619dcfe1794031145a36e
mtuan <tuanxsokoh@gmail.com>
2023-01-24 21:05:31 +0700

    added noti authkey

--- config/settings.example.js
+++ config/settings.example.js
@@ -32,4 +32,5 @@ export default {
     // The auth key to use for request header Authentication (optional). Set as undefined to disable authentication. 
     authKey: 'secret',
     notificationURL: 'https://example.com/notify',
+    notiAuthKey: 'secret',
 }
--- index.js
+++ index.js
@@ -53,11 +53,17 @@ for (let i = 0; i < settings.accounts.length; i++) {
 
     // call `api.refreshSession()` every hour to refresh the session
     const notiURL = settings?.notificationURL;
+    const notiRequest = axios.create({
+        baseURL: notiURL,
+        headers: {
+            'xva-access-token': settings.notiAuthKey || ""
+        }
+    });
     setInterval(() => {
         api.refreshSession().then(() => {
             console.log(`Session refreshed for account ${i}.`);
         }).catch((err) => {
-            notiURL && axios.post(notiURL, {
+            notiURL && notiRequest.post({
                 "type": "error",
                 "message": `Session refresh failed for account ${i}.`
             });
@@ -71,112 +77,113 @@ for (let i = 0; i < settings.accounts.length; i++) {
         api.resetSession().then(() => {
             console.log(`Session reset for account ${i}.`);
         }).catch((err) => {
-            notiURL && axios.post(notiURL, {
+            notiURL && axios.post({
                 "type": "error",
                 "message": `Session reset failed for account ${i}.`
             });
             throw err;
         });
     }, 24 * 60 * 60 * 1000);
-}
+    // create instance for post notification
 
-let currentAccountIndex = 0;
 
-const server = fastify();
+    let currentAccountIndex = 0;
 
-// configure CORS
-server.register(cors, {
-    origin: settings.corsOrigin || '*',
-});
+    const server = fastify();
 
-server.post('/conversation', async (request, reply) => {
-    // check for headers containing the API key
-    if (settings.authKey && request.headers['authorization'] !== settings.authKey) {
-        reply.code(401).send({ error: 'Unauthorized.' });
-        return;
-    }
+    // configure CORS
+    server.register(cors, {
+        origin: settings.corsOrigin || '*',
+    });
 
-    if (accounts.length === 0) {
-        reply.code(503).send({ error: 'No sessions available.' });
-        return;
-    }
-
-    let conversationId = undefined;
-    let messageId = undefined;
-
-    // search for conversationId and messageId using replyMsgId
-    if (request.body.replyMsgId) {
-        const row = db.getMessageByReplyMsgId(request.body.replyMsgId);
-        if (row) {
-            conversationId = row.conversationId;
-            messageId = row.messageId;
-        }
-    }
-
-    // Conversation IDs are tied to accounts, so we need to make sure that the same account is used for the same conversation.
-    if (conversationId) {
-        // get current account if already in the map
-        if (conversationsMap[conversationId]) {
-            currentAccountIndex = conversationsMap[conversationId];
-        } else {
-            const row = db.getAccountIdByConversationId(conversationId);
-            if (row) {
-                currentAccountIndex = accounts.findIndex((account) => account.account_id === row.accountId);
-                // set next account to create new conversation if not found available account
-                if (currentAccountIndex === -1) {
-                    currentAccountIndex = (currentAccountIndex + 1) % accounts.length;
-                    conversationId = undefined;
-                    messageId = undefined;
-                }
-            }
-        }
-    } else {
-        currentAccountIndex = (currentAccountIndex + 1) % accounts.length;
-    }
-
-    let result;
-    let error;
-    try {
-        result = await accounts[currentAccountIndex].sendMessage(request.body.message, {
-            conversationId,
-            parentMessageId: messageId,
-        });
-        // save if new conversationId 
-        if (!conversationsMap[result.conversationId]) {
-            conversationsMap[result.conversationId] = currentAccountIndex;
-            db.insertConversation(result.conversationId, accounts[currentAccountIndex].account_id);
-        }
-        result.response = result.response.trim();
-    } catch (e) {
-        error = e;
-    }
-
-    if (result !== undefined) {
-        reply.send(result);
-    } else {
-        console.error(error);
-        reply.code(503).send({ error: 'There was an error communicating with ChatGPT.' });
-    }
-});
-
-server.post('/message/register', async (request, reply) => {
-    try {
-        if (!request.body.conversationId || !request.body.messageId || !request.body.replyMsgId) {
-            reply.code(400).send({ error: 'Missing parameters.' });
-
+    server.post('/conversation', async (request, reply) => {
+        // check for headers containing the API key
+        if (settings.authKey && request.headers['authorization'] !== settings.authKey) {
+            reply.code(401).send({ error: 'Unauthorized.' });
             return;
         }
-        // insert into tbl_message
-        db.insertMessage(request.body.conversationId, request.body.messageId, request.body.replyMsgId);
-    } catch {
-        reply.code(503).send({ error: 'There was an error executing query to DB' });
-    }
-});
 
-server.listen({ port: settings.port || 3000, host: "0.0.0.0" }, (error) => {
-    if (error) {
-        console.error(error);
-        db.close();
-        process.exit(1);
-    }
-});
+        if (accounts.length === 0) {
+            reply.code(503).send({ error: 'No sessions available.' });
+            return;
+        }
+
+        let conversationId = undefined;
+        let messageId = undefined;
+
+        // search for conversationId and messageId using replyMsgId
+        if (request.body.replyMsgId) {
+            const row = db.getMessageByReplyMsgId(request.body.replyMsgId);
+            if (row) {
+                conversationId = row.conversationId;
+                messageId = row.messageId;
+            }
+        }
+
+        // Conversation IDs are tied to accounts, so we need to make sure that the same account is used for the same conversation.
+        if (conversationId) {
+            // get current account if already in the map
+            if (conversationsMap[conversationId]) {
+                currentAccountIndex = conversationsMap[conversationId];
+            } else {
+                const row = db.getAccountIdByConversationId(conversationId);
+                if (row) {
+                    currentAccountIndex = accounts.findIndex((account) => account.account_id === row.accountId);
+                    // set next account to create new conversation if not found available account
+                    if (currentAccountIndex === -1) {
+                        currentAccountIndex = (currentAccountIndex + 1) % accounts.length;
+                        conversationId = undefined;
+                        messageId = undefined;
+                    }
+                }
+            }
+        } else {
+            currentAccountIndex = (currentAccountIndex + 1) % accounts.length;
+        }
+
+        let result;
+        let error;
+        try {
+            result = await accounts[currentAccountIndex].sendMessage(request.body.message, {
+                conversationId,
+                parentMessageId: messageId,
+            });
+            // save if new conversationId 
+            if (!conversationsMap[result.conversationId]) {
+                conversationsMap[result.conversationId] = currentAccountIndex;
+                db.insertConversation(result.conversationId, accounts[currentAccountIndex].account_id);
+            }
+            result.response = result.response.trim();
+        } catch (e) {
+            error = e;
+        }
+
+        if (result !== undefined) {
+            reply.send(result);
+        } else {
+            console.error(error);
+            reply.code(503).send({ error: 'There was an error communicating with ChatGPT.' });
+        }
+    });
+
+    server.post('/message/register', async (request, reply) => {
+        try {
+            if (!request.body.conversationId || !request.body.messageId || !request.body.replyMsgId) {
+                reply.code(400).send({ error: 'Missing parameters.' });
+
+                return;
+            }
+            // insert into tbl_message
+            db.insertMessage(request.body.conversationId, request.body.messageId, request.body.replyMsgId);
+        } catch {
+            reply.code(503).send({ error: 'There was an error executing query to DB' });
+        }
+    });
+
+    server.listen({ port: settings.port || 3000, host: "0.0.0.0" }, (error) => {
+        if (error) {
+            console.error(error);
+            db.close();
+            process.exit(1);
+        }
+    });
